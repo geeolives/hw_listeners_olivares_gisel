da1a1417804e3929e2009cc160c078d19509516e
Gisel Olivares <giselpolivares@icloud.com>
2017-03-25 01:08:18 +0100

    added second event listener in js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-//This is my first JavaScript event listener attached to a button that listens for a click on the "Try It" button so that when it hears the event the listener gives you the exact time of day.//
+//This is my first JavaScript event listener attached to a button that listens for a click on the "Try It" button so that when it hears the event the listener gives you the exact time of day.
 
 document.getElementById('myButton').addEventListener('click', displayDate);
 
 function displayDate() {
     document.getElementById('demo').innerHTML = Date();
 }
+
+var img = document.getElementById('robot');
+
+//This is my second JavaScript event listener that listens for a mouseover the single image on my page. When the listener hears the event, the image changes to another of that of a unicorn gif.
+
+img.addEventListener('mouseover', function () {
+    if (img.src !== 'img/unicorn-loop.gif') {
+        img.src = 'img/unicorn-loop.gif';
+    }
+});
